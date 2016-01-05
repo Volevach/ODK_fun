6bc39eac0c0b0853025033db27cc80ceb8b52655
Volevach <v.volevach@gmail.com>
2016-01-06 00:09:19 +0100

    first build

--- FFT_Filter_RT/src_cg_priv/ODK_Functions.h
+++ FFT_Filter_RT/src_cg_priv/ODK_Functions.h
@@ -2,8 +2,8 @@
  * This file is ALWAYS GENERATED - DO NOT MODIFY this file.
  * This file contains the function prototypes the user defined.
  *
- * File created by ODK_CodeGenerator version 1.0.0.0
- * at Mi Juni 25 10:55:44 2014 
+ * File created by ODK_CodeGenerator version 101.0.902.1
+ * at Wed January 6 00:08:23 2016 
 */
 
 #if !defined    ODK_Functions_H
@@ -12,12 +12,13 @@
 #include "ODK_Types.h"
 
 #ifdef DLL_EXPORT
-    #define EXPORT_API extern "C" __declspec(dllexport)
+  #define EXPORT_API extern "C" __declspec(dllexport)
 #else
-    #define EXPORT_API extern "C"
+  #define EXPORT_API extern "C"
 #endif
-
 ODK_RESULT SampleFunction (const ODK_INT32& myInt, ODK_BOOL& myBool, ODK_DOUBLE& myReal);
 
-#endif // ODK_Functions_H
+ODK_RESULT GetTrace (const ODK_INT16& TraceCount, ODK_S7STRING TraceBuffer[256][127]);
+
+#endif // GetHashPrototype
 
