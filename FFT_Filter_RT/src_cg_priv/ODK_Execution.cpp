ab2a28b2b3f58fd29dfba9a511f00db6eff4047b
Volevach <v.volevach@gmail.com>
2016-01-17 23:06:58 +0100

    pssible bug: scl always has the gettrace function

--- FFT_Filter_RT/src_cg_priv/ODK_Execution.cpp
+++ FFT_Filter_RT/src_cg_priv/ODK_Execution.cpp
@@ -3,7 +3,7 @@
  * This file contains the execute function and the string helpers for ODK 1500S.
  *
  * File created by ODK_CodeGenerator version 2.0.0.0
- * at Sat January 16 22:46:11 2016
+ * at Sun January 17 22:46:35 2016
 */
 
 #include "ODK_Functions.h"
